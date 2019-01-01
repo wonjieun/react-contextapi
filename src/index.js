941c248a7deecbf31e673810bd8fc6b2be97aef0
wonjieun <wonjieunkk@gmail.com>
2019-01-01 13:10:26 +0900

    Basic context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import './typography';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+${reset};
+body {
+    background-color:#ecf0f1;
+}`;
+
+ReactDOM.render(
+  <>
+    <App />
+    <GlobalStyle />
+  </>,
+  document.getElementById('root')
+);
