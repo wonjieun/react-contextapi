9228492946a396a25ac1cb310d8cd67b3c51653d
wonjieun <wonjieunkk@gmail.com>
2019-01-01 23:51:45 +0900

    App Consumer 테스트

--- src/components/App/AppPresenter.js
+++ src/components/App/AppPresenter.js
@@ -2,12 +2,18 @@ import React, { Fragment } from 'react';
 import Header from 'components/Header';
 import Flex from 'styled-flex-component';
 import Notification from 'components/Notification';
+import Store from 'store';
 
 const AppPresenter = () => (
   <Fragment>
     <Header />
     <Flex alignCenter full column>
-      <Notification id={1} text={'Hello'} seen={false} />
+      <Store.Consumer>
+        {store => {
+          console.log(JSON.stringify(store));
+          return <Notification id={1} text={'Hello'} seen={false} />;
+        }}
+      </Store.Consumer>
     </Flex>
   </Fragment>
 );
