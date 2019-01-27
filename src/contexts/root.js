9a192fd69c3af55404ebc16c39a841b7ef3fbd88
wonjieun <wonjieunkk@gmail.com>
2019-01-27 14:37:05 +0900

    [CS] Default Provider

--- src/contexts/root.js
+++ src/contexts/root.js
@@ -4,8 +4,20 @@ const Context = React.createContext();
 const { Provider, Consumer: RootConsumer } = Context;
 
 class RootProvider extends Component {
+  state = {
+    value: 'myRoot'
+  };
+
+  actions = {
+    setValue: value => {
+      this.setState({ value });
+    }
+  };
+
   render() {
-    return <div />;
+    const { state, actions } = this;
+    const value = { state, actions };
+    return <Provider value={value}>{this.props.children}</Provider>;
   }
 }
 
